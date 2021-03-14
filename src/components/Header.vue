dba475074550f16a7897f7203a4444690e46a212
Cristopher <cristopher.p.s.cto@gmail.com>
2021-03-14 06:41:25 -0600

    Inits nue project with Vue 3 and Typescript

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <header class="text-gray-100 bg-gray-900 body-font shadow w-full">
+    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <nav class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
+        <a class="link">About</a>
+        <a class="link">Products</a>
+        <a class="link">Investor</a>
+      </nav>
+      <a
+        class="flex order-first
+        lg:order-none lg:w-1/5
+        title-font font-medium
+        items-center lg:items-center
+        lg:justify-center mb-4 md:mb-0"
+      >
+      <img src="https://pazly.dev/logo.png" style="height: 40px; margin-top: 10px; margin-bottom: 10px;" alt="logo">
+      <span class="ml-3 text-xl">pazly.dev</span>
+      </a>
+      <div class="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
+      <a href="https://www.buymeacoffee.com/pazlydev" class="bg-indigo-700 hover:bg-indigo-500 text-white ml-4 py-2 px-3 rounded-lg">
+        Donate
+      </a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Header',
+});
+</script>
+
+<style lang="postcss">
+.link {
+  @apply mr-5 hover:text-gray-900 cursor-pointer
+  border-b border-transparent hover:border-indigo-600;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home bg-white dark:bg-gray-800">
+    <transition name="fade">
+      <img v-if="true" alt="Vue logo" src="../assets/logo.png" width="500">
+    </transition>
+    <transition name="fade">
+      <div class="hello">
+        <h1>Welcome gdfgdfgdfg</h1>
+        <p>
+          For a guide and recipes on how to configure / customize this project,<br>
+          check out the
+          <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+        </p>
+      </div>
+    </transition>
+    <transition name="fade">
+      <h1>
+        <Autocounter
+          ref="counter"
+          :startAmount="500"
+          :endAmount="10000"
+          :duration="10000"
+          :autoinit="false"
+        />
+      </h1>
+    </transition>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Autocounter from 'vue3-autocounter'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    Autocounter,
+  },
+  mounted() {
+    this.$refs.counter.start();
+  },
+});
+</script>
