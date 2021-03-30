ed79191d2a93e5560b3d4d9da587eb0030010890
Cristopher <cristopher.p.s.cto@gmail.com>
2021-03-30 01:40:18 -0600

    Updates Sidebar styles

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,83 +1,45 @@
 <template>
   <div class="min-h-screen flex flex-row bg-white dark:bg-gray-900
-  h-full border-t-2 border-b-2">
-  <div class="hidden sm:flex flex-col w-56 bg-white border-r-2
+  h-full border-t-2 border-b-2 border-green-400 border-opacity-10">
+  <div class="hidden md:flex flex-col w-56 border-r border-green-400 border-opacity-10
   dark:bg-gray-900 overflow-hidden">
     <!-- <div class="flex items-center justify-center h-20 shadow-md">
       <h1 class="text-3xl uppercase text-indigo-500">Logo</h1>
     </div> -->
     <ul class="flex flex-col py-4">
       <li>
-        <a href="#" class="flex flex-row items-center h-12 transform
-        hover:translate-x-2 transition-transform ease-in duration-200
-        text-gray-500 hover:text-gray-800">
+        <a href="#" class="sidebar-link">
           <span class="inline-flex items-center justify-center h-12 w-12
           text-lg text-gray-400"><i class="bx bx-home"></i></span>
-          <span class="text-sm font-medium">Installantion</span>
+          <span class="text-lg font-medium">Installation</span>
         </a>
       </li>
       <li>
-        <a href="#" class="flex flex-row items-center h-12 transform
-        hover:translate-x-2 transition-transform ease-in duration-200
-        text-gray-500 hover:text-gray-800">
+        <a href="#" class="sidebar-link">
           <span class="inline-flex items-center justify-center h-12 w-12
           text-lg text-gray-400"><i class="bx bx-music"></i></span>
-          <span class="text-sm font-medium">Music</span>
+          <span class="text-lg font-medium">Usage</span>
         </a>
       </li>
       <li>
-        <a href="#" class="flex flex-row items-center h-12 transform
-        hover:translate-x-2 transition-transform ease-in duration-200
-        text-gray-500 hover:text-gray-800">
+        <a href="#" class="sidebar-link">
           <span class="inline-flex items-center justify-center h-12 w-12
           text-lg text-gray-400"><i class="bx bx-drink"></i></span>
-          <span class="text-sm font-medium">Drink</span>
+          <span class="text-lg font-medium">Properties</span>
         </a>
       </li>
       <li>
-        <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2
-        transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+        <a href="#" class="sidebar-link">
           <span class="inline-flex items-center justify-center h-12 w-12 text-lg
           text-gray-400"><i class="bx bx-shopping-bag"></i></span>
-          <span class="text-sm font-medium">Shopping</span>
+          <span class="text-lg font-medium">Events</span>
         </a>
       </li>
       <li>
-        <a href="#" class="flex flex-row items-center h-12 transform
-        hover:translate-x-2 transition-transform ease-in duration-200
-        text-gray-500 hover:text-gray-800">
+        <a href="#" class="sidebar-link">
           <span class="inline-flex items-center justify-center h-12 w-12
           text-lg text-gray-400"><i class="bx bx-chat"></i></span>
-          <span class="text-sm font-medium">Chat</span>
-        </a>
-      </li>
-      <li>
-        <a href="#" class="flex flex-row items-center h-12 transform
-        hover:translate-x-2 transition-transform ease-in duration-200
-        text-gray-500 hover:text-gray-800">
-          <span class="inline-flex items-center justify-center h-12 w-12
-          text-lg text-gray-400"><i class="bx bx-user"></i></span>
-          <span class="text-sm font-medium">Profile</span>
-        </a>
-      </li>
-      <li>
-        <a href="#" class="flex flex-row items-center h-12 transform
-        hover:translate-x-2 transition-transform ease-in duration-200
-        text-gray-500 hover:text-gray-800">
-          <span class="inline-flex items-center justify-center h-12
-          w-12 text-lg text-gray-400"><i class="bx bx-bell"></i></span>
-          <span class="text-sm font-medium">Notifications</span>
-          <span class="ml-auto mr-6 text-sm bg-red-100 rounded-full px-3
-          py-px text-red-500">5</span>
-        </a>
-      </li>
-      <li>
-        <a href="#" class="flex flex-row items-center h-12 transform
-        hover:translate-x-2 transition-transform ease-in duration-200
-        text-gray-500 hover:text-gray-800">
-          <span class="inline-flex items-center justify-center
-          h-12 w-12 text-lg text-gray-400"><i class="bx bx-log-out"></i></span>
-          <span class="text-sm font-medium">Logout</span>
+          <span class="text-lg font-medium">Methods</span>
         </a>
       </li>
     </ul>
@@ -93,3 +55,22 @@ export default defineComponent({
   name: 'Sidebar',
 });
 </script>
+
+<style lang="postcss" scoped>
+.sidebar-link {
+  @apply
+  flex flex-row items-center
+  h-10 text-xl transform hover:translate-x-2
+  transition-transform ease-in duration-200
+  text-gray-900 hover:text-green-500
+  dark:text-white dark:hover:text-green-500;
+}
+
+.link-active {
+  @apply
+  translate-x-2 text-green-500
+  border-b-0 border-l-4
+  bg-opacity-25 bg-green-400
+  dark:text-green-500;
+}
+</style>
