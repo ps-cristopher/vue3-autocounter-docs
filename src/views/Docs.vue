fa8ed26a4a7b6bda3a8c50de15731f0e357411fb
Cristopher <cristopher.p.s.cto@gmail.com>
2021-03-30 01:40:29 -0600

    Uodates docs

--- src/views/Docs.vue
+++ src/views/Docs.vue
@@ -1,7 +1,18 @@
 <template>
   <Sidebar>
-    <div class="flex flex-col w-full">
-      <h1>This is an about pagedgdfgdfgdfgdfgdfgdfgdf</h1>
+    <div class="flex flex-col w-full text-left px-4 py-4">
+      <p class="my-4">
+        A lightweight Vue 3 component made with TypeScript, you can use it to
+        create an animation that shows an automatic count, you can set any
+        quantity as well the count duration, it can be used for counting up
+        and down.
+      </p>
+      <h1 class="title">Installation</h1>
+      <pre class="code my-2">
+        <code>
+          npm install vue3-autocounter
+        </code>
+      </pre>
     </div>
   </Sidebar>
 </template>
@@ -21,3 +32,18 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="postcss">
+.title {
+  @apply
+  text-4xl font-bold
+  pl-2 pb-2
+  border-b-4 border-green-500;
+}
+
+.code {
+  position: relative;
+  background-color: #282c34;
+  border-radius: 6px;
+}
+</style>
