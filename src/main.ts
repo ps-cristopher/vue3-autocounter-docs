1d9d01cc8627a553a72e3db16f8ddb209dd72fed
Cristopher <cristopher.p.s.cto@gmail.com>
2021-04-23 18:38:10 -0500

    Imports CodeSnippet.vue and Vue3Autocounter as global components

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { createApp } from 'vue';
+import Vue3Autocounter from 'vue3-autocounter';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import CodeSnippet from './components/CodeSnippet.vue';
 
 createApp(App)
   .use(store)
   .use(router)
+  .component('code-snippet', CodeSnippet)
+  .component('vue3-autocounter', Vue3Autocounter)
   .mount('#app');
