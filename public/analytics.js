50ece1f1c5ec0f93b09ec5be6e8c4192f536b7fb
Cristopher <cristopher.p.s.cto@gmail.com>
2021-04-25 21:12:22 -0500

    Creates js file for google analytics

---
+++ public/analytics.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+/* eslint-disable prefer-rest-params */
+// Google Analytics
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+  dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+gtag('config', 'G-ZQ6N4W5WQW');
